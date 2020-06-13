2575ea90e234a4ad2224c230aa1cae74dd128012
Marcus <marcus_lee@u.nus.edu>
2020-06-13 21:42:26 +0800

    updated network parameters for better accuracy

--- posenet.js
+++ posenet.js
@@ -2,7 +2,7 @@ const posenet = require('@tensorflow-models/posenet');
 const tfnode  = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 const axios = require('axios');
-
+const regression = require('regression');
 
 const readImage = path => {
     const imageBuffer = fs.readFileSync(path);
@@ -14,17 +14,11 @@ const readImage = path => {
 async function estimatePoseOnImage(imageElement) {
     // load the posenet model from a checkpoint
     const net = await posenet.load({
-        architecture: 'MobileNetV1',
-        outputStride: 16,
-        inputResolution: { width: 257, height: 257 },
-        multiplier: 0.5 // For mobile
+        architecture: 'ResNet50',
+        outputStride: 32,
+        inputResolution: { width: 200, height: 200 },
+        quantBytes: 1
     });
-    // const net = await posenet.load({
-    //     architecture: 'ResNet50',
-    //     outputStride: 32,
-    //     inputResolution: { width: 257, height: 200 },
-    //     quantBytes: 2
-    // });
 
     const pose = await net.estimateSinglePose(imageElement, {
         flipHorizontal: false
@@ -32,7 +26,7 @@ async function estimatePoseOnImage(imageElement) {
     return pose;
 }
 
-function fallDectection(pose) {
+function thresholdFallDectection(pose) {
     const threshold = 0.1;
     const keypoints = pose.keypoints;
     let y_coordinates = 0;
@@ -44,17 +38,33 @@ function fallDectection(pose) {
         }
     }
     const avg_y = y_coordinates / counter;
-    return avg_y > (350);
+    return avg_y > 350;
+}
 
+function linRegFallDectection(pose) {
+    const threshold = 0.05;
+    const keypoints = pose.keypoints;
+    let coordinates = [];
+    for (p in keypoints) {
+        if (keypoints[p].score > threshold) {
+            const x = keypoints[p]['position']['x'];
+            const y = keypoints[p]['position']['y'];
+            coordinates.push([x,y]);
+        }
+    }
+    const linreg = regression.linear(coordinates);
+    const grad = Math.abs(linreg.equation[0]);
+    const r2 = linreg.r2;
+    return grad < 0.5;
 }
 
 const RaspiCam = require("raspicam");
 
 const camera = new RaspiCam({
     mode: "photo",
-    output: "/home/pi/hackathon/whitespace/images/photos_%d.jpg",
-    timeout: 100000,
-    timelapse: 1000,
+    output: "/home/pi/hackathon/whitespace/images/photos.jpg",
+    timeout: 1000 * 60 * 24 * 7,
+    timelapse: 4000,
     width: 640,
     height: 480,
     nopreview: true,
@@ -65,20 +75,21 @@ const camera = new RaspiCam({
 camera.start( );
 
 //listen for the "start" event triggered when the start method has been successfully initiated
-camera.on("start", function(){
+camera.on("start", function() {
     //do stuff
 });
 
 //listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){
+camera.on("read", function(err, timestamp, filename) {
     //do stuff
     const start = Date.now();
+    console.log(filename);
 
     const pose = estimatePoseOnImage(readImage(`images/${filename}`));
 
     pose.then(data => {
         // console.log(data);
-        const fall = fallDectection(data);
+        const fall = thresholdFallDectection(data);
         console.log('fall detected: : ' + fall);
         data.fall = fall;
         const json = JSON.stringify(data);
@@ -104,15 +115,16 @@ camera.on("read", function(err, timestamp, filename){
             console.log(err);
         });
 
+
 });
 
 //listen for the "stop" event triggered when the stop method was called
-camera.on("stop", function(){
+camera.on("stop", function() {
     //do stuff
 });
 
 //listen for the process to exit when the timeout has been reached
-camera.on("exit", function(){
+camera.on("exit", function() {
     //do stuff
 });
 
